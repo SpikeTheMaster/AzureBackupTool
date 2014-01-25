a884df264ef07ba5d013b1cafb02a5e1ebc235c1
Michael Wade <spikewade+github@gmail.com>
2014-01-25 16:49:03 +0000

    Working Version.

--- AzureBackupTool/AzureBackupTool.py
+++ AzureBackupTool/AzureBackupTool.py
@@ -1,32 +1,37 @@
 from azure.storage import BlobService
 import platform
 from datetime import datetime
+import subprocess
 
-regVPS = 'Backups\\VPSName'
+regVPS = 'backupsregvps'
+DOVPS = 'backupsredsearch'
+
+timeStamp = datetime.today().strftime("%Y-%m-%dT%H%M%SZ")
 
 pathToZip = ''
 sevenZip = ''
 fileName = 'Backup_'+timeStamp+'.7z'
-
-timeStamp = datetime.today().strftime("%Y-%m-%dT%H:%M:%SZ")
+activeContainer = ''
 
 if platform.system() == 'Windows' :
     pathToZip = 'C:\\SQLBackups\\' + fileName
     sevenZip = 'C:\\SQLBackups\\7Zip\\7za.exe'
+    activeContainer = regVPS
 else:
     # sudo apt-get install p7zip-full
     pathToZip = '/var/tmp/'+fileName
     sevenZip = '7z'
+    activeContainer = DOVPS
 
 #Create archive.7z containing Backups Directory with max compression
-zipArgs = "mx9 a "+ pathToZip +" Backups"
+zipArgs = "a "+ pathToZip +" Backups -mx9"
 
 subprocess.call(sevenZip + " " + zipArgs)
 
-blob_service = BlobService(account_name, account_key)
-blob_service.create_container('Backups')
+blob_service = BlobService('storageAccountName', 'storageKey')
 
 blob_service.create_container(regVPS)
+blob_service.create_container(DOVPS)
 
-blob_service.put_block_blob_from_path(regVPS, fileName, pathToZip)
+blob_service.put_block_blob_from_path(activeContainer, fileName, pathToZip)
 
